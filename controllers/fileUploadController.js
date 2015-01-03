7319d2b324be5345303d454018465b61c1003f5c
vishal-kamble <vishal26785@gmail.com>
2015-01-03 14:24:31 -0500

    file uploader

---
+++ controllers/fileUploadController.js
@@ -0,0 +1,26 @@
+var qs = require('querystring');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var request = require('request');
+var config = require('../config');
+
+ function fileUploadToAws (filePath, image){
+    var AWS = require('aws-sdk');
+
+    AWS.config.region= 'us-west-2';
+    AWS.config.update({accessKeyId: config.AWS_ACCESS_KEY, secretAccessKey: config.AWS_SECRET_KEY});
+
+    var s3 = new AWS.S3();
+    s3.putObject({
+        Bucket: config.AWS_BUCKET,
+        Key: filePath,
+        Body: image
+    }, function (err) {
+        if (err) {
+            throw err;
+        }
+    })
+
+};
+exports = module.exports = function(app){};
+exports.fileUploadToAws = fileUploadToAws;
