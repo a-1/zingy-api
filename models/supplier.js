fc6cf21ecb1a1c6e0a7d964fd1ff32680fe82c4e
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2015-01-03 00:41:36 -0500

    modified suppliers, facilities, events schema

--- models/supplier.js
+++ models/supplier.js
@@ -1,7 +1,19 @@
 var mongoose = require('mongoose');
 
 var supplierSchema = new mongoose.Schema({
-    title: {type: String, default: '', trim: true}
+    name: {type: String, default: ''},
+    imgUrl: {type: String, default: ''},
+    speciality: {type: String, default: ''},
+    description: {type: String, default: ''},
+    streetOne: {type: String, default: ''},
+    streetTwo: {type: String, default: ''},
+    city: {type: String, default: ''},
+    state: {type: String, default: ''},
+    pin: {type: String, default: ''},
+    email: {type: String, default: ''},
+    phoneOne: {type: String, default: ''},
+    phoneTwo: {type: String, default: ''},
+    url: {type: String, default: ''}
 });
 
 exports = module.exports = mongoose.model('Supplier', supplierSchema);
