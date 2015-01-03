54315843f5f25c24107b7048e3cac5c79adabeea
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2015-01-03 17:28:07 -0500

    modified coach schema

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ module.exports = {
     TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
     MONGO_URI: process.env.ZINGY_MONGO_URI || '',
     FACEBOOK_SECRET: process.env.ZINGY_FACEBOOK_SECRET || '',
-    GOOGLE_SECRET: process.env.ZINGY_GOOGLE_SECRET || ''
+    GOOGLE_SECRET: process.env.ZINGY_GOOGLE_SECRET || '',
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY || 'AKIAJJJT32VYZC3Y3FQA',
+    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY || '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',
+    AWS_BUCKET : process.env.AWS_BUCKET || 'demokheloapp'
 };
--- models/coach.js
+++ models/coach.js
@@ -1,8 +1,37 @@
 var mongoose = require('mongoose');
 
 var coachSchema = new mongoose.Schema({
-    sport: {type: String, default: '', trim: true},
-    experience: {type: Number, default: '', trim: true}
+    sports: [
+        {
+            name: String,
+            experienceYears: Number,
+            coachingMen: Boolean,
+            menFromAge: Number,
+            menToAge: Number,
+            menCharges: Number,
+            coachingWomen: Boolean,
+            womenFromAge: Number,
+            womenToAge: Number,
+            womenCharges: Number,
+            coachingKids: Boolean,
+            kidsFromAge: Number,
+            kidsToAge: Number,
+            kidsCharges: Number,
+            coachingFrequency: String,
+            startTime: {type: Date, default: ''},
+            endTime: {type: Date, default: ''},
+            streetOne: {type: String, default: ''},
+            streetTwo: {type: String, default: ''},
+            city: {type: String, default: ''},
+            state: {type: String, default: ''},
+            pin: {type: String, default: ''},
+            certificationName: {type: String, default: ''},
+            certificationBody: {type: String, default: ''},
+            certificationValidFrom: {type: Date, default: ''},
+            certificationValidTo: {type: Date, default: ''},
+            certificationImgUrl: {type: String, default: ''}
+        }
+    ]
 });
 
 exports = module.exports = mongoose.model('Coach', coachSchema);
