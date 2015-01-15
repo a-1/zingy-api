f07b6656f3eebd3a5ed981220025e1153e5bfbae
vishal-kamble <vishal26785@gmail.com>
2015-01-15 07:49:55 -0500

    file uploader

--- controllers/coachController.js
+++ controllers/coachController.js
@@ -1,13 +1,17 @@
 var mongoose = require('mongoose');
 var Coral = require('coral');
 var authController = require('./authController');
-
 module.exports = function (app) {
 
     var config = {
         path: '/api/coaches',
         model: mongoose.model('Coach'),
         middlewares: [authController.ensureAuthenticated],
+        query:{
+          options:{
+              populate:'profile'
+          }
+        },
         updateRef: {
             model: mongoose.model('User'),
             path: 'coach',
